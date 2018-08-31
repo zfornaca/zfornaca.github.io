6b92c08c6ea9be2fe6141bd2488b258c9a383ce1
Zack Fornaca <zfornaca@gmail.com>
2018-08-30 23:16:34 -0700

    ready for project insertions

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 /*
 TODO:
-@media burger in navbar
-portfolio should spread to own row @ middle width
 dropdown menu should be translucent/gauzy
 filter portfolio via Technologies listing?
 maybe click on photo to load new photo?
-animation to *slide* out the dropdown menu
-
+drop shadow on navbar
 
 
 */
