0689cdb420d5058260c361fc02311ab9832e3e49
Zack Fornaca <zfornaca@gmail.com>
2018-09-23 19:14:07 -0700

    contact buttons tweaked, drop shadow added

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ filter portfolio via Technologies listing?
 maybe click on photo to load new photo?
 drop shadow on navbar
 
+snake-ss.png
+https://github.com/zfornaca/Snake-game
+http://zackfornaca.com/Snake-game/
+
+jobbotron-ss.png
+https://github.com/zfornaca/Jobbotron
+
+hacksnooze-ss.png
+https://github.com/zfornaca/Hack-or-Snooze
+http://zackfornaca.com/Hack-or-Snooze/
 
 */
